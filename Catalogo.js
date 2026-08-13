fed64926e8d47239d8de04a7cf39bc52302f2578
guiloskinha <rafaelly.toroski@escola.pr.gov.br>
2026-08-13 09:19:01 -0300

    mn

--- Catalogo.js
+++ Catalogo.js
@@ -1,7 +1,8 @@
-export default class Catalogo {
-    #listaDeMidias;
-    constructor() {
-        this.#listaDeMidias = [];
+export default class Catalogo { // essa classepode ser exportada por outros arquivos j.s
+    #listaDeMidias; // declara uma propriedade e essa propriedade ela e privada e ela so pode ser acessada dentro da classe catalogo
+    constructor() { // e um metodo especial da Classe ela executa automaticamente
+        this.#listaDeMidias = []; // this e um objeto atual
+        // criar um array vazio, ou seja o catalogo comeca sem nenhum elemento
     }
     adicionar(midia) {
         this.#listaDeMidias.push(midia);
@@ -13,7 +14,12 @@ export default class Catalogo {
         container.innerHTML = "";
         this.#listaDeMidias.forEach(midia => {
 
-            container.innerHTML += midia.obterDad
-        })
+            container.innerHTML += midia.obterDadosHTML();
+        });
+    } // dia 13/08
+
+    static filtrarPorGenero(catalagoInstanciado, generoAlvo) {
+return catalagoInstanciado.#listaDeMidias.
+m => m.genero.tol
     }
 }
