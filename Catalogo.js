17d2f5979706548164183da2d388dec9b25cb43a
guiloskinha <rafaelly.toroski@escola.pr.gov.br>
2026-08-13 07:46:59 -0300

    bn

---
+++ Catalogo.js
@@ -0,0 +1,19 @@
+export default class Catalogo {
+    #listaDeMidias;
+    constructor() {
+        this.#listaDeMidias = [];
+    }
+    adicionar(midia) {
+        this.#listaDeMidias.push(midia);
+        this.renderizar();
+    }
+
+    renderizar() {
+        const container = document.getElementById("containerCatalogo");
+        container.innerHTML = "";
+        this.#listaDeMidias.forEach(midia => {
+
+            container.innerHTML += midia.obterDad
+        })
+    }
+}
